e93e3868236b745b0fab0a28f283001ab103ec4d
OwenZrk <owen.rodriguez.rojas@gmail.com>
2026-03-24 14:39:34 -0600

    Se termina la clase Modulos en Python

---
+++ modulos.py
@@ -0,0 +1,14 @@
+#Modulos
+
+from funciones import sum_two_values
+sum_two_values(5, 6)
+
+
+#import funciones
+#funciones.sum_two_values(5, 6)
+
+import math
+
+print(math.pi)
+print(math.pow(2, 7))
+
