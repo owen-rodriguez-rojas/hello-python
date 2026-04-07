868e8b294cfb18a9259780bbbf54c417cca0d0ac
OwenZrk <owen.rodriguez.rojas@gmail.com>
2026-04-07 06:25:17 -0600

    Se termina la clase de funciones de orden superior

---
+++ Intermedio/funciones_orden_superior.py
@@ -0,0 +1,58 @@
+from functools import reduce
+#Funciones de orden superior
+
+def sum_one(value):
+    return value + 1
+
+def sum_five(value):
+    return value + 5
+
+def sum_two_values_and_add_value(first_value, second_value, f_sum):
+    return f_sum(first_value + second_value)
+
+print(sum_two_values_and_add_value(5, 2, sum_five))
+print(sum_two_values_and_add_value(5, 2, sum_one))
+
+#Closures
+
+def sum_ten(original_value):
+    def add(value):
+        return value + 10 + original_value
+    return add
+add_closure = sum_ten(1)
+
+print(add_closure(5))
+print((sum_ten(5))(1))
+
+#Funciones de orden superior existentes
+
+numbers = [2, 5 , 10, 21, 2, 34]
+
+#Map
+
+def multiply_two(num):
+    return num * 2
+
+print(list(map(multiply_two, numbers)))
+print(list(map(lambda number: number*2, numbers)))
+
+#Filter
+
+def filter_greater_than_ten(num):
+    if num > 10:
+        return True
+    return False
+    
+    
+
+print(list(filter(filter_greater_than_ten, numbers)))
+print(list(filter(lambda num: num > 10, numbers)))
+
+#Reduce
+
+def sum_two_values(first, second):
+    print(first)
+    print(second)
+    return first + second 
+
+print(reduce(sum_two_values, numbers))
